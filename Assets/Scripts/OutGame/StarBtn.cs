c7fa0eddb1e5a7403d399d7f9bf6c6c0f4a6e1ac
이현우 <ihyeon-u@ihyeon-us-iMac.local>
2016-09-06 16:52:14 +0900

    아웃게임 장면 추가

---
+++ Assets/Scripts/OutGame/StarBtn.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StarBtn : MonoBehaviour {
+	public void GameStart() {
+		SceneManager.LoadScene ("InGame");
+	}
+}
