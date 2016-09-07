4df7ec99e36cdca498bd38058ee432f2aa92fd1e
이현우 <ihyeon-u@ihyeon-us-iMac.local>
2016-09-07 18:42:20 +0900

    게임 종료 메시지 작업

---
+++ Assets/Scripts/OutGame/OutGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class OutGame: MonoBehaviour {
+	public GameObject gameQuitPanel;
+
+	public void GameStart() {
+		SceneManager.LoadScene ("InGame");
+	}
+
+	public void OkGameQuit() {
+		Application.Quit ();
+	}
+
+	public void CancelGameQuit() {
+		gameQuitPanel.SetActive (false);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (gameQuitPanel.activeSelf) {			
+				gameQuitPanel.SetActive (false);
+			} else {
+				gameQuitPanel.SetActive (true);
+			}
+		}
+	}
+}
--- Assets/Scripts/OutGame/StarBtn.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using System.Collections;
-
-public class StarBtn : MonoBehaviour {
-	public void GameStart() {
-		SceneManager.LoadScene ("InGame");
-	}
-}
